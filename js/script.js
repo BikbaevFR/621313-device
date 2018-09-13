7ace71e547eda66d19fcd8aa050fa8523d921878
BikbaevFR <41439772+BikbaevFR@users.noreply.github.com>
2018-09-13 20:54:23 +0400

    Градиенты. Вроде все поправил.

--- js/script.js
+++ js/script.js
@@ -14,14 +14,14 @@
 	  popup.classList.remove("modal-show");
 	});
 
-	window.addEventListener("keydown", function(evt) {
-	  if (evt.keyCode === 27) {
-	    evt.preventDefault();
-	    if (popup.classList.contains("modal-show")) {
-	      popup.classList.remove("modal-show");
-	    }
-	  }
-	});
+	// window.addEventListener("keydown", function(evt) {
+	//   if (evt.keyCode === 27) {
+	//     evt.preventDefault();
+	//     if (popup.classList.contains("modal-show")) {
+	//       popup.classList.remove("modal-show");
+	//     }
+	//   }
+	// });
 
 	var mapLink = document.querySelector(".contacts-button-map");
 	var mapPopup = document.querySelector(".modal-map");
@@ -37,11 +37,11 @@
 	  mapPopup.classList.remove("modal-show");
 	});
 
-	window.addEventListener("keydown", function(evt) {
-	  evt.preventDefault();
-	  if (evt.keyCode === 27) {
-	    if (mapPopup.classList.contains("modal-show")) {
-	      mapPopup.classList.remove("modal-show");
-	    }
-	  }
-	});
+	// window.addEventListener("keydown", function(evt) {
+	//   evt.preventDefault();
+	//   if (evt.keyCode === 27) {
+	//     if (mapPopup.classList.contains("modal-show")) {
+	//       mapPopup.classList.remove("modal-show");
+	//     }
+	//   }
+	// });

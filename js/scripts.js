b4e15d9d73c4c130cfd233c580f92b52d5ac5a77
BikbaevFR <41439772+BikbaevFR@users.noreply.github.com>
2018-09-21 21:46:13 +0400

    Анимация ошибки через JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 var link = document.querySelector(".btn-write-us");
+
 var popup = document.querySelector(".modal-write-us");
 var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector(".write-us-form");
 var popupName = popup.querySelector(".write-us-name");
+var popupEmail = popup.querySelector(".write-us-email");
+var popupText = popup.querySelector(".write-us-text");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -12,6 +17,16 @@ link.addEventListener("click", function(evt) {
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!popupName.value || !popupEmail.value || !popupText.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } 
 });
 
 var mapLink = document.querySelector(".contacts-button-map");
